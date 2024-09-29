4a0b4cd3c051dcda88e1ba1c53fbd01d64098e72
devjasin <developer.jasin@gmail.com>
2024-09-30 01:37:50 +0545

    backend compete

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,18 @@
 import express from 'express'
 import globalErrorHandler from './middlewares/globalErrorHandler'
 import noteRoute from './note/noteRoute'
+import cors from 'cors'
+import envConfig from './config/config'
 
 
 const app= express()
 
 
 app.use(express.json())
+app.use(cors({
+  origin:envConfig.frontendUrl
+}))
+
 app.use("/api/notes",noteRoute)
 app.use(express.static("./src/uploads/"))
 app.use(globalErrorHandler)
--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -5,6 +5,7 @@ const envConfig={
   port:process.env.PORT,
   mongodbString:process.env.MONGODB_URI,
   backendurl:process.env.BACKEND_URL,
-  enviroment:process.env.NOTE_ENV
+  enviroment:process.env.NOTE_ENV,
+  frontendUrl:process.env.FRONTEND_URL
 }
 export default envConfig
